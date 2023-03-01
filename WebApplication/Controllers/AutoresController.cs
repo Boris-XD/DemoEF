514813c38d7ce126d938fe87c3c0fca69546fb46
Boris Sea <seaborisrc@gmail.com>
2023-02-28 22:41:06 -0400

    Start project with EF

---
+++ WebApplication/Controllers/AutoresController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DemoEF.Models;
+
+namespace DemoEF.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AutoresController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Autor>> Get()
+        {
+            return new List<Autor>()
+            {
+                new Autor()
+                {
+                    Id = 1,
+                    Name = "Test",
+                },
+                new Autor()
+                {
+                    Id = 2,
+                    Name = "Test 2",
+                }
+            };
+        }
+    }
+}
---
+++ WebApplication/DBContext/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using DemoEF.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoEF.DBContext
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options):base(options)
+        {
+
+        }
+        
+        public DbSet<Autor> Autores { get; set; } 
+    }
+}
